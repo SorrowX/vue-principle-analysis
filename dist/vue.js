cf334fe621935b541c9ab463d77140476480ab26
xuzhiwei <1356988206@qq.com>
2018-06-15 18:11:02 +0800

    异步组件

--- dist/vue.js
+++ dist/vue.js
@@ -2249,128 +2249,128 @@ function normalizeArrayChildren (children, nestedIndex) {
 /*  */
 
 function ensureCtor (comp, base) {
-  if (
-    comp.__esModule ||
-    (hasSymbol && comp[Symbol.toStringTag] === 'Module')
-  ) {
-    comp = comp.default;
-  }
-  return isObject(comp)
-    ? base.extend(comp)
-    : comp
+    if (
+        comp.__esModule ||
+        (hasSymbol && comp[Symbol.toStringTag] === 'Module')
+    ) {
+        comp = comp.default;
+    }
+    return isObject(comp)
+        ? base.extend(comp)
+        : comp
 }
 
 function createAsyncPlaceholder (
-  factory,
-  data,
-  context,
-  children,
-  tag
+    factory,
+    data,
+    context,
+    children,
+    tag
 ) {
-  var node = createEmptyVNode();
-  node.asyncFactory = factory;
-  node.asyncMeta = { data: data, context: context, children: children, tag: tag };
-  return node
+    var node = createEmptyVNode();
+    node.asyncFactory = factory;
+    node.asyncMeta = { data: data, context: context, children: children, tag: tag };
+    return node
 }
 
 function resolveAsyncComponent (
-  factory,
-  baseCtor,
-  context
+    factory,
+    baseCtor,
+    context
 ) {
-  if (isTrue(factory.error) && isDef(factory.errorComp)) {
-    return factory.errorComp
-  }
-
-  if (isDef(factory.resolved)) {
-    return factory.resolved
-  }
-
-  if (isTrue(factory.loading) && isDef(factory.loadingComp)) {
-    return factory.loadingComp
-  }
-
-  if (isDef(factory.contexts)) {
-    // already pending
-    factory.contexts.push(context);
-  } else {
-    var contexts = factory.contexts = [context];
-    var sync = true;
-
-    var forceRender = function () {
-      for (var i = 0, l = contexts.length; i < l; i++) {
-        contexts[i].$forceUpdate();
-      }
-    };
-
-    var resolve = once(function (res) {
-      // cache resolved
-      factory.resolved = ensureCtor(res, baseCtor);
-      // invoke callbacks only if this is not a synchronous resolve
-      // (async resolves are shimmed as synchronous during SSR)
-      if (!sync) {
-        forceRender();
-      }
-    });
-
-    var reject = once(function (reason) {
-      "development" !== 'production' && warn(
-        "Failed to resolve async component: " + (String(factory)) +
-        (reason ? ("\nReason: " + reason) : '')
-      );
-      if (isDef(factory.errorComp)) {
-        factory.error = true;
-        forceRender();
-      }
-    });
-
-    var res = factory(resolve, reject);
-
-    if (isObject(res)) {
-      if (typeof res.then === 'function') {
-        // () => Promise
-        if (isUndef(factory.resolved)) {
-          res.then(resolve, reject);
-        }
-      } else if (isDef(res.component) && typeof res.component.then === 'function') {
-        res.component.then(resolve, reject);
-
-        if (isDef(res.error)) {
-          factory.errorComp = ensureCtor(res.error, baseCtor);
-        }
-
-        if (isDef(res.loading)) {
-          factory.loadingComp = ensureCtor(res.loading, baseCtor);
-          if (res.delay === 0) {
-            factory.loading = true;
-          } else {
-            setTimeout(function () {
-              if (isUndef(factory.resolved) && isUndef(factory.error)) {
-                factory.loading = true;
-                forceRender();
-              }
-            }, res.delay || 200);
-          }
-        }
-
-        if (isDef(res.timeout)) {
-          setTimeout(function () {
-            if (isUndef(factory.resolved)) {
-              reject(
-                "timeout (" + (res.timeout) + "ms)"
-              );
-            }
-          }, res.timeout);
-        }
-      }
+    if (isTrue(factory.error) && isDef(factory.errorComp)) {
+        return factory.errorComp
     }
 
-    sync = false;
-    // return in case resolved synchronously
-    return factory.loading
-      ? factory.loadingComp
-      : factory.resolved
-  }
+    if (isDef(factory.resolved)) {
+        return factory.resolved
+    }
+
+    if (isTrue(factory.loading) && isDef(factory.loadingComp)) {
+        return factory.loadingComp
+    }
+
+    if (isDef(factory.contexts)) {
+        // already pending
+        factory.contexts.push(context);
+    } else {
+        var contexts = factory.contexts = [context];
+        var sync = true;
+
+        var forceRender = function () {
+            for (var i = 0, l = contexts.length; i < l; i++) {
+                contexts[i].$forceUpdate();
+            }
+        };
+
+        var resolve = once(function (res) {
+            // cache resolved
+            factory.resolved = ensureCtor(res, baseCtor);
+            // invoke callbacks only if this is not a synchronous resolve
+            // (async resolves are shimmed as synchronous during SSR)
+            if (!sync) {
+                forceRender();
+            }
+        });
+
+        var reject = once(function (reason) {
+            "development" !== 'production' && warn(
+                "Failed to resolve async component: " + (String(factory)) +
+                (reason ? ("\nReason: " + reason) : '')
+            );
+            if (isDef(factory.errorComp)) {
+                factory.error = true;
+                forceRender();
+            }
+        });
+
+        var res = factory(resolve, reject);
+
+        if (isObject(res)) {
+            if (typeof res.then === 'function') {
+                // () => Promise
+                if (isUndef(factory.resolved)) {
+                    res.then(resolve, reject);
+                }
+            } else if (isDef(res.component) && typeof res.component.then === 'function') {
+                res.component.then(resolve, reject);
+
+                if (isDef(res.error)) {
+                    factory.errorComp = ensureCtor(res.error, baseCtor);
+                }
+
+                if (isDef(res.loading)) {
+                    factory.loadingComp = ensureCtor(res.loading, baseCtor);
+                    if (res.delay === 0) {
+                        factory.loading = true;
+                    } else {
+                        setTimeout(function () {
+                            if (isUndef(factory.resolved) && isUndef(factory.error)) {
+                                factory.loading = true;
+                                forceRender();
+                            }
+                        }, res.delay || 200);
+                    }
+                }
+
+                if (isDef(res.timeout)) {
+                    setTimeout(function () {
+                        if (isUndef(factory.resolved)) {
+                            reject(
+                              "timeout (" + (res.timeout) + "ms)"
+                            );
+                        }
+                    }, res.timeout);
+                }
+            }
+        }
+
+        sync = false;
+        // return in case resolved synchronously
+        return factory.loading
+            ? factory.loadingComp
+            : factory.resolved
+    }
 }
 
 /*  */
